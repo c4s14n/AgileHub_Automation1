f1dab8ab99af7bee90d5ecd0c9e0740ed8302ffe
Casian <casian.stupari@gmail.com>
2020-10-14 20:52:20 +0300

    14_10_2020-OOPBasic

---
+++ DemoApp/DemoApp/Curs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    public static class Curs
+    {
+        public static int CursId { get; set; }
+        public static string Nume { get; set; }
+        public  static int Durata { get; set; }
+
+        //public Curs(int cursId,string nume)
+        //{
+        //    CursId = cursId;
+        //    Nume = nume;
+        //}
+
+        //public Curs(string nume, int durata)
+        //{
+        //    Nume = nume;
+        //    Durata = durata;
+
+        //}
+
+        public static void SetareCurs(int id, string name)
+        {
+            CursId = id;
+            Nume = Nume;
+
+        }
+
+
+
+
+    }
+}
--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -10,12 +10,14 @@ namespace DemoApp
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+          //Curs curstulTestAutomation= new Curs(1,"Testare Automatizata");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+          //Curs cursulJavaDev= new Curs("Java Development",12);
+          
+            
+         HomePage myHomePage= new HomePage();
+
+
         }
     }
 }
